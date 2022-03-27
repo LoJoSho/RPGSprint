be42f4871db7d1c2a1f4b8e62b6e628b6363785d
LoJoSho <lojoshogaming@gmail.com>
2022-03-27 11:03:57 -0500

    Added Reload Command

---
+++ src/main/java/me/lojosho/rpgsprint/commands/CommandRPGSprint.java
@@ -0,0 +1,30 @@
+package me.lojosho.rpgsprint.commands;
+
+import me.lojosho.rpgsprint.RPGSprint;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.jetbrains.annotations.NotNull;
+
+public class CommandRPGSprint implements CommandExecutor {
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        if (args.length == 1) {
+            switch (args[0]) {
+                case "reload":
+                    if (sender.hasPermission("RPGSprint.reload")) {
+                        RPGSprint.getInstance().setupPlugin();
+                        return true;
+                    } else {
+                        sender.sendMessage(Component.text("No Permission!").color(NamedTextColor.RED));
+                    }
+                    break;
+            }
+        } else {
+            sender.sendMessage("Do /rpgsprint reload");
+        }
+        return false;
+    }
+}
